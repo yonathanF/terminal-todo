6194c594e16777889d11f336ce7fa8ffe4f82932
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-05-15 00:21:29 -0400

    the major python files are added

---
+++ core/scheduler.py
@@ -0,0 +1,11 @@
+def linear_priority_function(due_date, priority_level, fake_bost=0):
+    ''' a function that ranks a task's priority '''
+    pass
+
+    def calculate_relative_priority(
+            self, priority_function=linear_priority_function):
+        '''
+        calculates the tasks priority given a priority function
+        The function defaults to a a linear function, as a sane option.
+        '''
+        pass
