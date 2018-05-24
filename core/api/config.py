75783ec3759d3f90eedd933e3800a8611c02837f
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-05-24 16:22:30 -0600

    a file to hold the global config values....not sure if this is the best way to do it

---
+++ core/api/config.py
@@ -0,0 +1,4 @@
+CLIENT_ID = "9662f098a73140eebc7f1fdb9efac61f"
+CLIENT_SECRET = "df93a09ff8ec49b8beba8e6a8e23f5f6"
+AUTH_URL = "https://todoist.com/oauth/authorize"
+TOKEN_URL = "https://todoist.com/oauth/access_token"
