532c1c9640663726dfec252370449dbb6bae91b7
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-05-25 20:09:23 -0600

    added basic terminal args

--- core/term_todoist.py
+++ core/term_todoist.py
@@ -14,13 +14,36 @@ def main():
 
     # set the description
     arg_parser = argparse.ArgumentParser(
-        description=
-        'A simple terminal client for Todoist that supports all functionality of the web application.'
-    )
+        prog="term_todoist",
+        description='A simple terminal client for Todoist that\
+        supports all functionality of the web application.')
 
-    # add task argument
+    # add task
     arg_parser.add_argument(
-        'integer', metavar='N', type=int, nargs="+", help="some help")
+        '-a',
+        type=str,
+        help="Add a task. Add time near the end\
+            with a -t [usual string time]")
+
+    # edit task
+    arg_parser.add_argument(
+        '-e',
+        type=str,
+        help="Edit a task. Enter task id followed by the change")
+
+    # list tasks a label
+    arg_parser.add_argument(
+        '-ll', type=str, help="List all tasks with these tag/s")
+
+    # list tasks with project
+    arg_parser.add_argument(
+        '-lp', type=str, help="List all tasks under this project")
+
+    # auth argument
+    arg_parser.add_argument(
+        '-auth',
+        type=bool,
+        help="Go through the OAuth process. Only need to do it once.")
 
     args = arg_parser.parse_args()
 
